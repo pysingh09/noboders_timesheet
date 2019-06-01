6ebe831e6105389a541bb304907211ac2e303c69
yashdeep212 <ytiwari212>
2019-06-01 19:21:36 +0530

    update setting

--- thoughtwin_erp/settings/development.py
+++ thoughtwin_erp/settings/development.py
@@ -12,6 +12,17 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = PROJECT_APPS.child("media")
 MEDIA_URL = '/media/'
 
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     'erp',
+        'USER':     'postgres',
+        'PASSWORD': 'psql',
+        'HOST':     'localhost',
+        'PORT':     5432,
+    }
+}
+
 # local settings
 try:
     from .local import *
