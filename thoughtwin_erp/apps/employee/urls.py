0ad227980ad9ddc9195a4018d2908b810c11c60c
preetam pawar <preetam@thoughtwin.com>
2019-06-18 17:41:12 +0530

    menu name changes

--- thoughtwin_erp/apps/employee/urls.py
+++ thoughtwin_erp/apps/employee/urls.py
@@ -14,14 +14,13 @@ urlpatterns = [
     path('update/<int:pk>/', EditProfileView.as_view(), name="update"),
     path('leave/employeelist/', LeaveListView.as_view(), name='leave_list'),
     path('profile/<int:pk>/', ListOfProfile.as_view(), name="profile_list"),
+    
     path('leaves/', LeaveCreateView.as_view(), name="leaves"),
     path('update_leave/<int:pk>/', EditAllotedLeaveView.as_view(), name="update_leave"),
     
     path('show/calendar/<int:id>', show_calendar,name ='show_calendar'),
     path('show/calendar', show_hour_calender,name ='show-hour-calender'),
 
-    # path('show/', leave_calendar,name ='calendar'),
-    
     path('request/leave/', request_leave,name ='request_leave'),
     path('employee_details/<int:id>/', employee_details, name='employee_details'),
     path('attendence/date-time-attendence/diff', date_time_attendence_view, name='date-time-attendence-view'),
@@ -29,7 +28,7 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('', index, name='index'),
     path('dashboard', Dashboard.as_view(), name='dashboard' ),
-    path('Approved/leave', approved_leave, name='approved_leave' ),
+    path('approved/leave', approved_leave, name='approved_leave' ),
     
 
 
