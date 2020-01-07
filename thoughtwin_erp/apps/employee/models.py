f761f933c7c5e57df1261b9e7dc4a4074677a545
lenovo 4 <iamdeveloper-me>
2020-01-07 11:53:22 +0530

    add seven_n_half working_time

--- thoughtwin_erp/apps/employee/models.py
+++ thoughtwin_erp/apps/employee/models.py
@@ -69,11 +69,15 @@ ROLE_CHOICES = (
     (9 , ('Junior Developer')),  
 )
 WORKING_TIME_CHOICES = ( 
-    (6, ('6')),
-    (7, ('7')),
-    (8, ('8')),
-    (9 , ('9')),
+    (1, ('6:00')),
+    (2,('6:30')),  
+    (3, ('7:00')),
+    (4,('7:30')),
+    (5, ('8:00')),
+    (6,('8:30')),
+    (7, ('9:00')),
 )
+
 class BaseModel(models.Model):
    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
    modified_at = models.DateTimeField(auto_now=True, db_index=True)
@@ -90,7 +94,7 @@ class Profile(BaseModel):
     profile_image = models.ImageField(upload_to = 'pic_folder/', default = 'avatar-mini-2.jpg')
     teamlead = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('Teamlead'), related_name='teamlead')
     designation = models.IntegerField(choices=ROLE_CHOICES, default=7, verbose_name=_('Designation'))
-    working_time = models.IntegerField(choices=WORKING_TIME_CHOICES,default=9, verbose_name=_('Working Time'))
+    working_time = models.IntegerField(choices=WORKING_TIME_CHOICES, default=7, verbose_name=_('Working Time'))
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile_created_by')
 
     def __str__(self):
