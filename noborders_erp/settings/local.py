0dcc8c4bbf516e920588e02a751380cf65fa7ceb
Ashutosh <ashutosh.webllisto@gmail.com>
2020-03-05 16:33:43 +0530

    refactor: add local.py to gitignore

--- noborders_erp/settings/local.py
+++ noborders_erp/settings/local.py
@@ -2,28 +2,26 @@ from .base import *
 
 
 DATABASES = {
-   'default': {
-       'ENGINE':   'django.db.backends.postgresql',
-       'NAME':     'erp_dev1',
-       'USER':     'postgres',
-       'PASSWORD': 'psql',
-       'HOST':     'localhost',
-       'PORT':     5432,
-   }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "thoughtwin_erp",
+        "USER": "ashutosh",
+        "PASSWORD": "adminadmin",
+        "HOST": "localhost",
+        "PORT": 5432,
+    }
 }
-
 
 
 # EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
 # FROM_EMAIL='ytiwari212@gmail.com'
 # EMAIL_BACKEND = "anymail.backends.sendinblue.EmailBackend"
 # ANYMAIL = {
-    
+
 #     "SENDINBLUE_API_KEY": "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",
 # }
 
 
-
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # FROM_EMAIL='erp.thoughtwin@gmail.com'
 # EMAIL_USE_TLS = True
@@ -40,7 +38,7 @@ DATABASES = {
 # EMAIL_HOST_PASSWORD = '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'
 # EMAIL_PORT = 587
 
-FROM_EMAIL='erp.thoughtwin@gmail.com'
+FROM_EMAIL = "erp.thoughtwin@gmail.com"
 # EMAIL_BACKEND = 'django_ses.SESBackend'
 # AWS_ACCESS_KEY_ID = 'AKIAQYKYEQYHWH555FXN'
 # AWS_SECRET_ACCESS_KEY = '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'
