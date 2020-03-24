595b3befbcf62bac0b12e56cfe30d6b71b11d4f2
Hasib <hasib.webllisto@gmail.com>
2020-03-25 02:05:40 +0530

    update: heroku deployment settings

--- noborders_erp/settings/production.py
+++ noborders_erp/settings/production.py
@@ -1,3 +1,4 @@
+import dj_database_url
 from .base import *
 import os,sys
 
@@ -30,13 +31,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = PROJECT_ROOT.split("/settings")[0] + "/static"
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = PROJECT_DIR.child("collected_static")
