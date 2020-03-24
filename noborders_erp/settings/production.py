3be9b7c5298ce36df97746e8ac5764cd5c1c5591
Hasib <hasib.webllisto@gmail.com>
2020-03-25 01:38:05 +0530

    feat: heroku deployment settings

--- noborders_erp/settings/production.py
+++ noborders_erp/settings/production.py
@@ -1,36 +1,59 @@
 from .base import *
 import os,sys
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hobby-dev',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
+
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
-    'default': {
-        'ENGINE':   'django.db.backends.postgresql',
-        'NAME':     'erp',
-        'USER':     'postgres',
-        'PASSWORD': 'admin@#$123',
-        'HOST':     'localhost',
-        'PORT':     5432,
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = PROJECT_DIR.child("collected_static")
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    PROJECT_APPS.child("static"),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
-MEDIA_ROOT = PROJECT_APPS.child("media")
-MEDIA_URL = '/media/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = PROJECT_DIR.child("collected_static")
+# STATICFILES_DIRS = (
+#     PROJECT_APPS.child("static"),
+# )
+
+# MEDIA_ROOT = PROJECT_APPS.child("media")
+# MEDIA_URL = '/media/'
 
 
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-FROM_EMAIL='rathoreutkarsh0699@gmail.com'
-EMAIL_BACKEND = 'django_ses.SESBackend'
-AWS_ACCESS_KEY_ID = 'AKIAQYKYEQYHWH555FXN'
-AWS_SECRET_ACCESS_KEY = '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'
+# FROM_EMAIL='rathoreutkarsh0699@gmail.com'
+# EMAIL_BACKEND = 'django_ses.SESBackend'
+# AWS_ACCESS_KEY_ID = 'AKIAQYKYEQYHWH555FXN'
+# AWS_SECRET_ACCESS_KEY = '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'
 
 
 
